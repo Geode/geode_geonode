61c939696ce0a1762871932707185213ec8bce00
Raphael Sprumont <raphael.sprumont@opengeode.be>
2014-10-17 11:35:09 +0200

    initial commit

---
+++ geode_geonode/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from geonode.urls import *
+
+urlpatterns = patterns('',
+
+    # Static pages
+    url(r'^$', TemplateView.as_view(template_name='site_index.html'), name='home'),
+ ) + urlpatterns
